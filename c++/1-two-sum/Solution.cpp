ba30a49112509f13f9f9c11fc063bc87587c609e
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-01-13 18:03:26 -0500

    Finished two sums

---
+++ c++/1-two-sum/Solution.cpp
@@ -0,0 +1,33 @@
+//
+// https://leetcode.com/url-to-problem
+//
+#include <vector>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> value_to_index;
+        int first_index = 0;
+        int second_index = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int num = nums[i];
+            int diff = target - num;
+
+            auto potential_index_ite = value_to_index.find(diff);
+            if (potential_index_ite != value_to_index.end()) {
+                first_index = potential_index_ite->second;
+                second_index = i;
+                break;
+            }
+
+            value_to_index[num] = i;
+        }
+
+        return { first_index, second_index };
+    }
+};
