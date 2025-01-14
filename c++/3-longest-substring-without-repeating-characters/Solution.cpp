b7042a704e09ee66b3bd3576f8dad3f9e2dfed4a
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-01-14 16:09:17 -0500

    Modified solution for problem 3 and main

--- c++/3-longest-substring-without-repeating-characters/Solution.cpp
+++ c++/3-longest-substring-without-repeating-characters/Solution.cpp
@@ -1,14 +1,42 @@
 //
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/?envType=problem-list-v2&envId=oizxjoit
+// https://leetcode.com/problem-list/oizxjoit/
 //
 #include <vector>
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        unordered_set<char> our_set = {};
+        int longest_substring = 0;
 
+        int current_substring = 0;
+        for (unsigned int i = 0; i < s.size(); ++i) {
+            for (unsigned int j = i; j < s.size(); ++j) {
+                int old_set_size = our_set.size();
+                our_set.insert(s[j]);
+                int new_set_size = our_set.size();
+
+                if (current_substring > longest_substring) {
+                    longest_substring = current_substring;
+                }
+
+                if (old_set_size == new_set_size) {
+                    our_set.clear();
+                    current_substring = 1;
+                    break;
+                }
+
+                ++current_substring;
+            }
+
+            current_substring = 0;
+        }
+
+        return max(longest_substring, current_substring);
     }
 };
+
--- c++/main.cpp
+++ c++/main.cpp
@@ -1,3 +1,12 @@
+#include "./3-longest-substring-without-repeating-characters/Solution.cpp"
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    auto s = Solution().lengthOfLongestSubstring("aa");
+
+    cout << s << endl;
+
     return 0;
 }
