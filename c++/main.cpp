399b564ce2a906578986b8f261b2597f8a69c311
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-02-03 18:36:21 -0500

    Utils for next list nodes problems

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,10 +1,16 @@
-#include "./141-linked-list-cycle/Solution.cpp"
+#include "./23-merge-k-sorted-lists/Solution.cpp"
 #include <iostream>
+#include "./utils/ListNode.h"
 
 using namespace std;
 
 int main() {
-    auto s = Solution().hasCycle({});
+    vector<ListNode*> lists = {
+        make_list_node_from_values({1, 4, 5}),
+        make_list_node_from_values({1, 3, 4}),
+        make_list_node_from_values({2, 6})};
+
+    auto s = Solution().mergeKLists(lists);
     cout << s << endl;
 
     return 0;
--- c++/utils/ListNode.h
+++ c++/utils/ListNode.h
@@ -13,4 +13,32 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+inline void print_list_node(ListNode* node) {
+    ListNode* traversal = node;
+    std::cout << "{ ";
+    while (traversal != nullptr) {
+        std::cout << traversal->val << " ";
+        traversal = traversal->next;
+    }
+    std::cout << "}" << std::endl;
+}
+
+inline ListNode* make_list_node_from_values(const std::vector<int>& values) {
+    if (values.empty()) {
+        return nullptr;
+    }
+
+    ListNode* node = new ListNode(values[0]);
+    ListNode* traversal = node;
+    for (int i = 1; i < values.size(); ++i) {
+        traversal->next = new ListNode(values[i]);
+        traversal = traversal->next;
+    }
+
+    std::cout << "- Creating list node: ";
+    print_list_node(node);
+
+    return node;
+}
+
 #endif //LISTNODE_H
