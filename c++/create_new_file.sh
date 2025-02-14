4d372dcdbecad7b570918af6b8c50d1b96a70b20
MathBeaudoin <mathisbeaudoin15@hotmail.com>
2025-02-14 15:33:06 -0500

    script to create directory for + solution file for new leetcode problems

---
+++ c++/create_new_file.sh
@@ -0,0 +1,7 @@
+echo "--- Creating a new directory for a C++ leetcode question ---"
+echo "- What should be the name of the directory?"
+read answer
+mkdir $answer
+
+touch $answer/Solution.cpp
+cat 0-template-problem/Solution.cpp > $answer/Solution.cpp
